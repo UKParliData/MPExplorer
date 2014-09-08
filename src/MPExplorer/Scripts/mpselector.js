2bc79f46ac5c48bf7c97187f25d17e1d75e6bb9c
Wojciech Stawiarski <stawiarskiw@parliament.uk>
2014-09-08 11:35:03 +0100

    added basic dropdown list of MPs

---
+++ src/MPExplorer/Scripts/mpselector.js
@@ -0,0 +1,28 @@
+﻿(function () {
+    "use strict";
+
+    var mpSelect = function () {
+        var self = this;
+
+        self.names = ko.observableArray();
+
+        self.names.push({
+            id: 0,
+            name: "abc 1"
+        });
+
+        self.names.push({
+            id: 1,
+            name: "def 2"
+        });
+
+        self.names.push({
+            id: 3,
+            name: "ghi 1"
+        });
+    }
+
+    
+
+    ko.applyBindings(new mpSelect(), document.getElementById("body"));
+})();
